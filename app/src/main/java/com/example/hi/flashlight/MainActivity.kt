259693f01140478fbf46985e035cc20590c8976b
cafealpha <cyanine@naver.com>
2018-11-26 16:26:14 +0900

    기본 구현

--- app/src/main/java/com/example/hi/flashlight/MainActivity.kt
+++ app/src/main/java/com/example/hi/flashlight/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.hi.flashlight
-
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val torch = Torch(this)
+
+        flashSwitch.setOnCheckedChangeListener{
+            buttonView, isChecked ->
+            if(isChecked){
+                torch.flashOn()
+            }else{
+                torch.flashOff()
+            }
+        }
+
     }
+
+
+
 }
